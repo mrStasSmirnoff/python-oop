a3545ad3e608f01d5875a9e8f4675243580af753
mrStasSmirnoff <mr.StasSmirnoff@hotmail.com>
2023-12-09 13:15:11 +0100

    working on __init___

--- oop-freecode-camp/main.py
+++ oop-freecode-camp/main.py
@@ -5,7 +5,10 @@ link: https://www.youtube.com/watch?v=Ej_02ICOIgs&ab_channel=freeCodeCamp.org
 
 
 class Item:
+
     pay_rate = 0.8 # class attribute (20% discount)
+    all = list()
+
     def __init__(self, name: str, price: float, quantity):
 
         assert price >= 0, f"Price {price} is not greater than zero!"
@@ -16,28 +19,27 @@ class Item:
         self.quantity = quantity
         print(f"An instance created: {name}")
 
+        # Actions to execute
+
+        Item.all.append(self)
+
 
     def calc_total_price(self):
         return self.price * self.quantity
 
+
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
+
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+
+
 item1 = Item("Phone", 100, 1)
 item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 
-#print(item1.calc_total_price())
-#print(item2.calc_total_price())
-#print(Item.pay_rate) # Accessing class attribute
-#print(item1.pay_rate) # class attribute is also available on instance level
-#print(item2.pay_rate)
-
-#print(Item.__dict__) # All the attributes for class level
-#print(item1.__dict__) # All the attributes for instance level
-
-item1.apply_discount()
-print(item1.price)
-
-item2.pay_rate = 0.7
-item2.apply_discount()
-print(item2.price)
+print(Item.all)
